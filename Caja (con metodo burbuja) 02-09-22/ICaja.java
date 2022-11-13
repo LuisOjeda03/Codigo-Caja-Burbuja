8d048d915664dbc996920dca794f797e7c0af8fe
Luis Enrique Ojeda Lopez <79891690+LuisOjeda03@users.noreply.github.com>
2022-11-13 14:53:16 -0600

    Add files via upload

---
+++ Caja (con metodo burbuja) 02-09-22/ICaja.java
@@ -0,0 +1,9 @@
+package primer_parcial;
+public interface ICaja 
+{
+	public abstract boolean insertar (int valor);
+	public abstract void eliminar (int indice);
+	public abstract void ordenar ();
+	public abstract boolean buscar (int valor);
+	public abstract void mostrarDatos ();
+}
